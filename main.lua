e32a107d1fb95137349da0ef05a599e81c848d82
javck <javck.zack@gmail.com>
2015-08-10 12:29:56 +0800

    補充註解

--- main.lua
+++ main.lua
@@ -1,18 +1,15 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--	這個範例展示如何使用Geometric Shapes相關的API，更多display相關API請參考以下網址
+--  https://docs.coronalabs.com/api/library/display/index.html
+--	Author: Zack Lin
+--	Time: 2015/3/12
 -----------------------------------------------------------------------------------------
-
---[[
-	這個範例展示如何使用Geometric Shapes相關的API
-	Author: Zack Lin
-	Time: 2015/3/12
-]]
 
 --畫一條線，從座標100,100到200,0
 local line = display.newLine( 100,100,200,0 )
-line:setColor(255,255,255)
+--設定線條的顏色，RGB數字為R/255,G/255,B/255
+line:setColor(1,1,1)
+--設定線條的寬度
 line.width = 3
 --從上次劃線的終點，繼續依序劃過300,100和100,100
 line:append(300,100,100,100)
@@ -22,9 +19,12 @@ local squre = display.newRect( 100, 100, 200, 200 )
 --因為預設的錨點為中央，改設定為左上角
 squre.anchorX = 0
 squre.anchorY = 0
+--設定筆畫寬度
 squre.strokeWidth = 3
-squre:setFillColor( 255,0,0 )
-squre:setStrokeColor( 255,255,255 )
+--設定填滿顏色
+squre:setFillColor( 1,0,0 )
+--設定筆畫顏色
+squre:setStrokeColor( 1,1,1 )
 
 local rect = display.newRect(  220, 230 , 50, 70 )
 rect.anchorX = 0
